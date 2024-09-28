9bb5041e59047d5e4c8abb46d92c31ef527c90c3
Tomas Daniel <tomdaniel@email.cz>
2024-09-28 21:51:53 +0200

    Add login and logout functionality.

--- code/project/urls.py
+++ code/project/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Home page
-    path('', views.home, name='home')
+    # Home page + Login
+    path('', views.home, name='home'),
+    path('logout/', views.client_logout, name='logout')
 ]
--- code/project/views.py
+++ code/project/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.utils.safestring import mark_safe
 
 # Home view
 def home(request):
+    # If POST request, user tries to log in, otherwise would be GET
+    if request.method == "POST":
+        # Store given credentials
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Authenticate user
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        # Check if successful
+        if user is not None:
+            login(user)
+            messages.success(request, mark_safe(f"Welcome back {username}!"))
+        else:
+            messages.error(request, mark_safe(f"Error with Your login <strong>{username}</strong>, please try again..."))
+        # Redirect back to homepage
+        return redirect('home')
     return render(request, 'home.html', {})
+
+# Perform current user logout
+def client_logout(request):
+    logout(request)
+    messages.info(request, "Logout successful, see You soon")
+    # Redirect back to homepage
+    return redirect('home')
