f1478486effd7809933437673d229a281230f5f5
Tomas Daniel <tomdaniel@email.cz>
2024-09-28 17:49:12 +0200

    Introduce more reasonable folder naming.

--- code/project/apps.py
+++ code/project/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IisprojectConfig(AppConfig):
+class ProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'iisproject'
+    name = 'project'
