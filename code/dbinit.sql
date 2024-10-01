808280f8b1d8195f7e4b767f45e6f012af2a454c
Milan Vrbas <milan.vrbas1@gmail.com>
2024-10-01 23:43:22 +0200

    DB adjustments after MySQL testing

--- code/dbinit.sql
+++ code/dbinit.sql
@@ -3,80 +3,33 @@
 -- pozn.    VARCHAR2 a NVARCHAR/NVARCHAR2 ve v MySQL nepoužívají
 --          používám VARCHAR(255) - VARCHAR může mít proměnlivou délku
 
------------------------------------------------------------------------------------------------
------------------------------------------ DROP TABLE ------------------------------------------
------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------- --
+-- --------------------------------------- DROP TABLE -------------------------------------- --
+-- ----------------------------------------------------------------------------------------- --
 
-DROP TABLE IF EXISTS Zvire;
-DROP TABLE IF EXISTS Plemeno;
-DROP TABLE IF EXISTS Zdravotni_zaznam;
-DROP TABLE IF EXISTS Fotka_zvirete;
+-- Důležité je zachování správného pořadí DROP TABLE (dependency)
 
-DROP TABLE IF EXISTS Uzivatel;
-DROP TABLE IF EXISTS Veterinar;
-DROP TABLE IF EXISTS Administrator;
-DROP TABLE IF EXISTS Dobrovolnik;
-DROP TABLE IF EXISTS Pecovatel;
-
-DROP TABLE IF EXISTS Prohlidka;
 DROP TABLE IF EXISTS Venceni;
+DROP TABLE IF EXISTS Prohlidka;
 DROP TABLE IF EXISTS Rezervace;
 
------------------------------------------------------------------------------------------------
----------------------------------------- CREATE TABLE -----------------------------------------
------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS Zdravotni_zaznam;
+DROP TABLE IF EXISTS Fotka_zvirete;
+DROP TABLE IF EXISTS Zvire;
 
-CREATE TABLE Zvire (
-    ID_zvirete INT AUTO_INCREMENT not NULL,
-    Druh VARCHAR(255) not NULL,
-    Jmeno VARCHAR(255) not NULL,
-    Pohlavi TINYINT not NULL, -- 0 = samec(muž), 1 = samice(žena)
-    Datum_narozeni DATE, -- může být NULL, nevíme kdy se zvíře narodilo a jeho věk
-    Datum_prichodu DATE not NULL,
-    Aktivni BOOLEAN not NULL, -- false = není v útulku, true = je v útulku
-    Popis TEXT not NULL,
+DROP TABLE IF EXISTS Dobrovolnik;
+DROP TABLE IF EXISTS Administrator;
+DROP TABLE IF EXISTS Veterinar;
+DROP TABLE IF EXISTS Pecovatel;
+DROP TABLE IF EXISTS Uzivatel;
 
-    PRIMARY KEY(ID_zvirete),
+DROP TABLE IF EXISTS Plemeno;
 
-    ID_plemene INT, -- cizí klíč na tabulku Plemeno
-    FOREIGN KEY(ID_plemene) REFERENCES Plemeno(ID_plemene) -- Vazba na Plemeno
-);
+-- ----------------------------------------------------------------------------------------- --
+-- -------------------------------------- CREATE TABLE ------------------------------------- --
+-- ----------------------------------------------------------------------------------------- --
 
-CREATE TABLE Plemeno (
-    ID_plemene INT AUTO_INCREMENT not NULL,
-    Nazev VARCHAR(255) not NULL,
-
-    PRIMARY KEY(ID_plemene)
-);
-
-CREATE TABLE Zdravotni_zaznam (
-    ID_zaznamu INT AUTO_INCREMENT not NULL,
-    Nazev VARCHAR(255) not NULL,
-    Detail TEXT not NULL,
-
-    PRIMARY KEY(ID_zaznamu),
-
-    ID_zvirete INT not NULL,
-    FOREIGN KEY(ID_zvirete) REFERENCES Zvire(ID_zvirete) ON DELETE CASCADE,
-        -- když se smaže zvíře, potom se smažou i jeho záznamy
-
-    ID_veterinare INT not NULL, -- odkazuje na ID_uzivatele z tabulky Uzivatel (Veterinar)
-    FOREIGN KEY(ID_veterinare) REFERENCES Uzivatel(ID_uzivatele)
-        -- při smazání veterináře nechceme aby se odstranily i záznamy
-);
-
--- <<week>> entita --
-CREATE TABLE Fotka_zvirete (
-    ID_fotky INT AUTO_INCREMENT not NULL,
-    Cesta_k_obrazku VARCHAR(255) not NULL,
-
-    PRIMARY KEY(ID_fotky),
-
-    ID_zvirete INT not NULL, -- cizí klíč na zvíře
-    FOREIGN KEY(ID_zvirete) REFERENCES Zvire(ID_zvirete) ON DELETE CASCADE
-        -- odstranění fotek, pokud je zvíře odstraněno
-);
-
+-- ---------------------------------- VYTVOŘENÍ UŽIVATELE ---------------------------------- --
 CREATE TABLE Uzivatel (
     ID_uzivatele INT AUTO_INCREMENT not NULL,
     Jmeno VARCHAR(255) not NULL,
@@ -129,6 +82,61 @@ CREATE TABLE Veterinar (
     FOREIGN KEY(ID_uzivatele) REFERENCES Uzivatel(ID_uzivatele) ON DELETE CASCADE
 );
 
+-- ----------------------------------- VYTVOŘENÍ ZVÍŘETE ----------------------------------- --
+
+CREATE TABLE Plemeno (
+    ID_plemene INT AUTO_INCREMENT not NULL,
+    Nazev VARCHAR(255) not NULL,
+
+    PRIMARY KEY(ID_plemene)
+);
+
+CREATE TABLE Zvire (
+    ID_zvirete INT AUTO_INCREMENT not NULL,
+    Druh VARCHAR(255) not NULL,
+    Jmeno VARCHAR(255) not NULL,
+    Pohlavi TINYINT not NULL, -- 0 = samec(muž), 1 = samice(žena)
+    Datum_narozeni DATE, -- může být NULL, nevíme kdy se zvíře narodilo a jeho věk
+    Datum_prichodu DATE not NULL,
+    Aktivni BOOLEAN not NULL, -- false = není v útulku, true = je v útulku
+    Popis TEXT not NULL,
+
+    PRIMARY KEY(ID_zvirete),
+
+    ID_plemene INT, -- cizí klíč na tabulku Plemeno
+    FOREIGN KEY(ID_plemene) REFERENCES Plemeno(ID_plemene) -- Vazba na Plemeno
+);
+
+-- <<week>> entita --
+CREATE TABLE Fotka_zvirete (
+    ID_fotky INT AUTO_INCREMENT not NULL,
+    Cesta_k_obrazku VARCHAR(255) not NULL,
+
+    PRIMARY KEY(ID_fotky),
+
+    ID_zvirete INT not NULL, -- cizí klíč na zvíře
+    FOREIGN KEY(ID_zvirete) REFERENCES Zvire(ID_zvirete) ON DELETE CASCADE
+        -- odstranění fotek, pokud je zvíře odstraněno
+);
+
+CREATE TABLE Zdravotni_zaznam (
+    ID_zaznamu INT AUTO_INCREMENT not NULL,
+    Nazev VARCHAR(255) not NULL,
+    Detail TEXT not NULL,
+
+    PRIMARY KEY(ID_zaznamu),
+
+    ID_zvirete INT not NULL,
+    FOREIGN KEY(ID_zvirete) REFERENCES Zvire(ID_zvirete) ON DELETE CASCADE,
+        -- když se smaže zvíře, potom se smažou i jeho záznamy
+
+    ID_veterinare INT not NULL, -- odkazuje na ID_uzivatele z tabulky Uzivatel (Veterinar)
+    FOREIGN KEY(ID_veterinare) REFERENCES Uzivatel(ID_uzivatele)
+        -- při smazání veterináře nechceme aby se odstranily i záznamy
+);
+
+-- ---------------------------------- VYTVOŘENÍ REZERVACE ---------------------------------- --
+
 CREATE TABLE Rezervace (
     ID_rezervace INT AUTO_INCREMENT not NULL,
     Zacatek DATETIME not NULL, -- datum + čas
@@ -169,4 +177,17 @@ CREATE TABLE Prohlidka (
     -- při smazání veterináře se smaže i rezervace
 );
 
--- TODO: naplnit to datama
+-- ----------------------------------------------------------------------------------------- --
+-- ----------------------------------- INSERT INTO TABLE ----------------------------------- --
+-- ----------------------------------------------------------------------------------------- --
+
+-- Databáze se naplní ukázkovými daty, slouží pro usnadnění vývoje (snad)
+
+-- ---------------------------------- INSERT PRO ZVÍŘÁTKA ---------------------------------- --
+-- TODO
+
+-- ---------------------------------- INSERT PRO UŽIVATELE---------------------------------- --
+-- TODO
+
+-- ---------------------------------- INSERT PRO REZERVACE---------------------------------- --
+-- TODO
