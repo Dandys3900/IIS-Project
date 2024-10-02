8b0c8f89cfc0261cdd519449001ecf981951f255
Tomas Daniel <tomdaniel@email.cz>
2024-10-02 20:33:18 +0200

    Setup static files serving for production.

--- code/source/settings.py
+++ code/source/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,8 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Set path for static files for local server
+STATICFILES_DIRS = [
+    BASE_DIR / 'project' / 'static'
+]
+# Set path for static files in production (can't be same as previous path)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_ROOT = 'project/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
