abcd0187f9288abfa145dcefa5c72ad55445e127
Tomas Daniel <tomdaniel@email.cz>
2024-10-03 09:15:02 +0200

    Add grid with animal cards to home page and move login to separate page.

--- code/project/views.py
+++ code/project/views.py
@@ -4,8 +4,11 @@ from django.contrib import messages
 from django.utils.safestring import mark_safe
 from .forms import SignUpForm
 
-# Home view
 def home(request):
+    return render(request, 'home.html', {})
+
+# Perform client login
+def client_login(request):
     # If POST request, user tries to log in, otherwise would be GET
     if request.method == "POST":
         # Store given credentials
@@ -25,7 +28,7 @@ def home(request):
             messages.error(request, mark_safe(f"Error with Your login <strong>{username}</strong>, please try again..."))
         # Redirect back to homepage
         return redirect('home')
-    return render(request, 'home.html', {})
+    return render(request, 'login.html', {})
 
 # Perform current user logout
 def client_logout(request):
