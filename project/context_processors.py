8d63615f4f051eebe22dad7e56a7c7af3c68ddb9
Tomas Daniel <52777834+Dandys3900@users.noreply.github.com>
2024-11-10 22:46:36 +0100

    Verify volunteers (#9)
    * Implemenet desired behaviour.
    
    * Add missing favicon.
    Add text to determine volunteer verification status.
    
    * Extend information shown in volunteers table to help carer decide whether to verify or not.

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,17 +1,22 @@
 from .forms import UserInfoForm
+from .models import CustomUser
 
 def userdetails_form(request):
     form = None
     user = request.user
-    pendingTasksCount = 0
+    pendingTasksCount  = 0
+    pending_volunteers = 0
 
     # Create form for logged-in user only
     if user.is_authenticated:
         form = UserInfoForm(user=user)
         if user.userRole() == "vet":
             pendingTasksCount = user.assigned_tasks.filter(is_done=False).count()
+        elif user.userRole() == "carer":
+            pending_volunteers = CustomUser.objects.filter(userrole="volunteer", verified=False).count()
     # Return form
     return {
-        "userdetails_form" : form,
-        "assigned_tasks"   : pendingTasksCount
+        "userdetails_form"   : form,
+        "assigned_tasks"     : pendingTasksCount,
+        "pending_volunteers" : pending_volunteers
     }
